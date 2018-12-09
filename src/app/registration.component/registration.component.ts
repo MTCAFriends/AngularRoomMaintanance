7d28c26cf27a55fe642f57c226cad6199538c890
devarajamca@gmail.com <devarajamca@gmail.com>
2018-12-09 12:12:09 +0530

    validation2

--- src/app/registration.component/registration.component.ts
+++ src/app/registration.component/registration.component.ts
@@ -1,5 +1,5 @@
   import { Component, OnInit } from '@angular/core';
-  //import { FormGroup, FormBuilder,Validators } from '@angular/forms';
+  import { FormGroup, FormBuilder,Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -8,31 +8,28 @@
 })
 export class RegistrationComponent implements OnInit {
 
-  addUser(regForm){
-    console.log(regForm.value);
-  }
-    
+  regForm:FormGroup;
   constructor() { 
-    // this.regForm=this.fb.group({
-    //   name:['',[Validators.required,Validators.minLength(2),Validators.maxLength(10)]],
-    //   sname:['',[Validators.required,Validators.minLength(2),Validators.maxLength(10)]],
-    //   dob:'',
-    //   gender:'',
-    //   emailCtrl:'',
-    //   pwd:'',
-    //   conpwd:'',
-    //   mobile:'',
-    //   aadhar:'',
-
-    // })
-
+    
   }
-  // onSubmit(){
-  //   console.log(this.regForm.value);
-  // }
-  genderList:string[]=['Male','Female','Other'];
-
+   addUser(regForm){
+     console.log(regForm.value);
+     //console.log(this.regForm.value);
+   }
   ngOnInit() {
+    this.regForm=new FormGroup({
+      name:new FormControl('',[Validators.required,Validators.pattern('[a-zA-Z][a-zA-Z]+')]),
+      sname:new FormControl('',[Validators.required,Validators.pattern('[a-zA-Z][a-zA-Z]+')]),
+      dob:new FormControl('',[Validators.required]),
+      gender:new FormControl('',),
+      email:new FormControl('',[Validators.required,Validators.email]),
+      pwd:new FormControl('',[Validators.required]),
+      cnfpass:new FormControl('',[Validators.required]),
+      mobile:new FormControl('',[Validators.required,Validators.pattern('[0-9]{10}'),Validators.maxLength(10)]),
+      aadhar:new FormControl('',[Validators.required,Validators.pattern('[0-9]{12}'),Validators.maxLength(12)]),
+      address:new FormControl('',[Validators.required,Validators.minLength(10),Validators.maxLength(100)]),
+
+    });
   }
 
 }
